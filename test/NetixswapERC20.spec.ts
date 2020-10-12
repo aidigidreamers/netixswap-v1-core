15ca3242670dc06dfaeaab0576946271045b0c42
aidigidreamers <61518519+aidigidreamers@users.noreply.github.com>
2020-10-12 22:21:37 +0500

    Update NetixswapERC20.spec.ts

--- test/NetixswapERC20.spec.ts
+++ test/NetixswapERC20.spec.ts
@@ -30,7 +30,7 @@ describe('NetixswapERC20', () => {
   it('name, symbol, decimals, totalSupply, balanceOf, DOMAIN_SEPARATOR, PERMIT_TYPEHASH', async () => {
     const name = await token.name()
     expect(name).to.eq('Netixswap')
-    expect(await token.symbol()).to.eq('Netix')
+    expect(await token.symbol()).to.eq('NETIX')
     expect(await token.decimals()).to.eq(18)
     expect(await token.totalSupply()).to.eq(TOTAL_SUPPLY)
     expect(await token.balanceOf(wallet.address)).to.eq(TOTAL_SUPPLY)
