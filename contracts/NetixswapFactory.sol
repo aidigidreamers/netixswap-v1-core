3a1774ad281cd34df1328b9cc8b3b71be9bd14d5
aidigidreamers <61518519+aidigidreamers@users.noreply.github.com>
2020-10-12 20:44:44 +0500

    master

---
+++ contracts/NetixswapFactory.sol
@@ -0,0 +1,49 @@
+pragma solidity =0.5.16;
+
+import './interfaces/INetixswapFactory.sol';
+import './NetixswapPair.sol';
+
+contract NetixswapFactory is INetixswapFactory {
+    address public feeTo;
+    address public feeToSetter;
+
+    mapping(address => mapping(address => address)) public getPair;
+    address[] public allPairs;
+
+    event PairCreated(address indexed token0, address indexed token1, address pair, uint);
+
+    constructor(address _feeToSetter) public {
+        feeToSetter = _feeToSetter;
+    }
+
+    function allPairsLength() external view returns (uint) {
+        return allPairs.length;
+    }
+
+    function createPair(address tokenA, address tokenB) external returns (address pair) {
+        require(tokenA != tokenB, 'Netixswap: IDENTICAL_ADDRESSES');
+        (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
+        require(token0 != address(0), 'Netixswap: ZERO_ADDRESS');
+        require(getPair[token0][token1] == address(0), 'Netixswap: PAIR_EXISTS'); // single check is sufficient
+        bytes memory bytecode = type(NetixswapPair).creationCode;
+        bytes32 salt = keccak256(abi.encodePacked(token0, token1));
+        assembly {
+            pair := create2(0, add(bytecode, 32), mload(bytecode), salt)
+        }
+        INetixswapPair(pair).initialize(token0, token1);
+        getPair[token0][token1] = pair;
+        getPair[token1][token0] = pair; // populate mapping in the reverse direction
+        allPairs.push(pair);
+        emit PairCreated(token0, token1, pair, allPairs.length);
+    }
+
+    function setFeeTo(address _feeTo) external {
+        require(msg.sender == feeToSetter, 'Netixswap: FORBIDDEN');
+        feeTo = _feeTo;
+    }
+
+    function setFeeToSetter(address _feeToSetter) external {
+        require(msg.sender == feeToSetter, 'Netixswap: FORBIDDEN');
+        feeToSetter = _feeToSetter;
+    }
+}
